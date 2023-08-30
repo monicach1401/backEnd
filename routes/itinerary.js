90e61ce251102f200ecd3f3356a62a04c5056c30
Monica Chavarria <monicachjob@gmail.com>
2023-08-30 12:23:48 +0200

    firstCommit

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -7,14 +7,14 @@ router.get('/test', (req, res) => {
     res.send({ msg: 'itinerarys test route.' });
   });
 
-/*get all itinerarys*/
+/*get all itinerarys
 router.get('/all', (req, res) => {
   itineraryModel.find()
     .then(files => {
       res.send(files);
     })
     .catch(err => console.log(err));
-});
+});*/
 
 
 
